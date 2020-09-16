54c7b4daa7529c8ff4809a410b4bda14752acb87
jreahl <jreahl@wellesley.edu>
2020-09-16 16:40:58 -0700

    Make aesthetic edits to sensitivity test figures

--- SENSITIVITY-TEST.py
+++ SENSITIVITY-TEST.py
@@ -12,9 +12,8 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
-from matplotlib.patches import Patch, Ellipse, Rectangle
+from matplotlib.patches import Ellipse, Rectangle
 import matplotlib.transforms as transforms
-from matplotlib.lines import Line2D
 
 # Import ALLDATA.csv
 lower = pd.read_csv('Data_CSV/ALLDATA_NARTISS-LOWER.csv')
@@ -33,76 +32,6 @@ tex_allauthors = ['as', 'cf', 'cg', 'er', 'ls', 'pf', 'saf', 'slf', 'vc',
                   'low', 'med', 'high']
 tex_mechanical = ['as', 'cf', 'cg', 'er', 'ls', 'saf', 'slf', 'vc', 'low',
                   'med', 'high']
-
-
-class RepackagedData:
-    '''
-    Class to reformat input data for the PCA plotting functions.
-    '''
-    def __init__(self, data, tex):
-        self.data = data.loc[:, tex]
-        self.transportcolor = list(map(lambda s: s.replace('\ufeff', ''),
-                                   data.transportcolor))
-        self.climatecolor = list(map(lambda s: s.replace('\ufeff', ''),
-                                 data.climatecolor))
-        self.marker = data.marker
-
-
-
-def textplot(ax, tex, x, y, PCx, PCy, label):
-    """
-    Plot text on PCA plots so that microtexture abbreviations do not overlap
-    with arrows.
-    ----------
-    ax = axis object (e.g. ax1, ax2, etc.)
-    tex = microtextures used to make PCA ordination
-    x = float; x coordinate of vector
-    y = float; y coordinate of vector
-    PCx = integer; indicate principal component that x-axis is using
-          PC1 = 0, PC2 = 1, PC3 = 2
-    PCy = integer; indicate principal component that y-axis is using
-          PC1 = 0, PC2 = 1, PC3 = 2
-    label = string; text label
-    """
-    adj = 0.01  # Adjustment factor
-    s = 16
-    p = len(tex)
-    b_k = np.zeros(p)
-    total = np.zeros(p)
-    for i in range(p):
-        for j in range(i+1, p+1):
-            total[i] += 1/j
-        b_k[i] = (1/p)*total[i]
-    if np.square(x) > b_k[PCx] or np.square(y) > b_k[PCy]:
-        if x > 0 and y > 0:
-            ax.text(x, y+adj, label, size=s, weight='black')
-        if x > 0 and y < 0:
-            ax.text(x, y-adj, label, size=s, weight='black')
-        if x < 0 and y < 0:
-            if x < -0.6:
-                ax.text(-0.6, y+adj, label, size=s, weight='black')
-            else:
-                ax.text(x-(adj*5), y-(adj*2), label, size=s, weight='black')
-        if x < 0 and y > 0:
-            if x < -0.55:
-                ax.text(-0.55, y+adj, label, size=s, weight='black')
-            else:
-                ax.text(x-(adj*5), y+adj, label, size=s, weight='black')
-    else:
-        if x > 0 and y > 0:
-            ax.text(x, y+adj, label, size=s)
-        if x > 0 and y < 0:
-            ax.text(x, y-adj, label, size=s)
-        if x < 0 and y < 0:
-            if x < -0.6:
-                ax.text(-0.6, y+adj, label, size=s)
-            else:
-                ax.text(x-(adj*5), y-(adj*2), label, size=s)
-        if x < 0 and y > 0:
-            if x < -0.55:
-                ax.text(-0.55, y+adj, label, size=s)
-            else:
-                ax.text(x-(adj*5), y+adj, label, size=s)
 
 
 def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
@@ -258,14 +187,14 @@ def Sensitivityplot(datalist, tex, label):
             if i == 0:
                 ax[i, j].set_xlabel('PC1', size=20)
                 ax[i, j].text(-4.5, 4.5, 'A'+str(j+1), size=40)
-                # for k in range(len(data_m)):
-                #         fk = data_m['color'].loc[k]
-                #         mk = data_m['marker'].loc[k]
-                #         xk = data_m['PC1'].loc[k]
-                #         yk = data_m['PC2'].loc[k]
-                #         ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
-                #                           edgecolors=fk, s=200, alpha=0.5,
-                #                           linewidths=1)
+                for k in range(len(data_m)):
+                    fk = data_m['color'].loc[k]
+                    mk = data_m['marker'].loc[k]
+                    xk = data_m['PC1'].loc[k]
+                    yk = data_m['PC2'].loc[k]
+                    ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
+                                          edgecolors=fk, s=200, alpha=0.5,
+                                          linewidths=1)
                 colors = ['#D55E00', '#0072B2', '#F0E442']
                 for c in colors:
                     trans_group = data_m[data_m['color'] == c]
@@ -274,16 +203,17 @@ def Sensitivityplot(datalist, tex, label):
                                            n_std=2, facecolor='none',
                                            edgecolor=c, alpha=1, lw=2)
                 for k in range(len(data_a)):
-                    fk = data_a['color'].loc[k]
-                    mk = data_a['marker'].loc[k]
-                    xk = data_a['PC1'].loc[k]
-                    yk = data_a['PC2'].loc[k]
                     if data_a['author'].loc[k] == 'Nartiss_2017':
-                        sk = 1000
-                    elif data_a['author'].loc[k] != 'Nartiss_2017':
-                        sk = 200
-                    ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
-                                         edgecolors='k', s=sk, alpha=1,
+                        fk = data_a['color'].loc[k]
+                        mk = data_a['marker'].loc[k]
+                        xk = data_a['PC1'].loc[k]
+                        yk = data_a['PC2'].loc[k]
+                    # if data_a['author'].loc[k] == 'Nartiss_2017':
+                    #     sk = 1000
+                    # elif data_a['author'].loc[k] != 'Nartiss_2017':
+                    #     sk = 200
+                        ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
+                                         edgecolors='k', s=500, alpha=1,
                                          linewidths=1)
                 if j == 0:
                     ax[i, j].set_ylabel('PC2', size=20)
@@ -321,14 +251,14 @@ def Sensitivityplot(datalist, tex, label):
             if i == 1:
                 ax[i, j].set_xlabel('PC1', size=20)
                 ax[i, j].text(-4.5, 4.5, 'B'+str(j+1), size=40)
-                # for k in range(len(data_m)):
-                #         fk = data_m['color'].loc[k]
-                #         mk = data_m['marker'].loc[k]
-                #         xk = data_m['PC1'].loc[k]
-                #         yk = data_m['PC3'].loc[k]
-                #         ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
-                #                           edgecolors=fk, s=200, alpha=0.5,
-                #                           linewidths=1)
+                for k in range(len(data_m)):
+                    fk = data_m['color'].loc[k]
+                    mk = data_m['marker'].loc[k]
+                    xk = data_m['PC1'].loc[k]
+                    yk = data_m['PC3'].loc[k]
+                    ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
+                                          edgecolors=fk, s=200, alpha=0.5,
+                                          linewidths=1)
                 colors = ['#D55E00', '#0072B2', '#F0E442']
                 for c in colors:
                     trans_group = data_m[data_m['color'] == c]
@@ -336,16 +266,17 @@ def Sensitivityplot(datalist, tex, label):
                                        ax[i, j], n_std=2, facecolor='none',
                                        edgecolor=c, alpha=1, lw=2)
                 for k in range(len(data_a)):
-                    fk = data_a['color'].loc[k]
-                    mk = data_a['marker'].loc[k]
-                    xk = data_a['PC1'].loc[k]
-                    yk = data_a['PC3'].loc[k]
                     if data_a['author'].loc[k] == 'Nartiss_2017':
-                        sk = 1000
-                    elif data_a['author'].loc[k] != 'Nartiss_2017':
-                        sk = 200
-                    ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
-                                         edgecolors='k', s=sk, alpha=1,
+                        fk = data_a['color'].loc[k]
+                        mk = data_a['marker'].loc[k]
+                        xk = data_a['PC1'].loc[k]
+                        yk = data_a['PC3'].loc[k]
+                    # if data_a['author'].loc[k] == 'Nartiss_2017':
+                    #     sk = 1000
+                    # elif data_a['author'].loc[k] != 'Nartiss_2017':
+                    #     sk = 200
+                        ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
+                                         edgecolors='k', s=500, alpha=1,
                                          linewidths=1)
                 if j == 0:
                     ax[i, j].set_ylabel('PC3', size=20)
@@ -360,14 +291,14 @@ def Sensitivityplot(datalist, tex, label):
             if i == 2:
                 ax[i, j].set_xlabel('PC2', size=20)
                 ax[i, j].text(-4.5, 4.5, 'C'+str(j+1), size=40)
-                # for k in range(len(data_m)):
-                #         fk = data_m['color'].loc[k]
-                #         mk = data_m['marker'].loc[k]
-                #         xk = data_m['PC2'].loc[k]
-                #         yk = data_m['PC3'].loc[k]
-                #         ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
-                #                           edgecolors=fk, s=200, alpha=0.5,
-                #                           linewidths=1)
+                for k in range(len(data_m)):
+                    fk = data_m['color'].loc[k]
+                    mk = data_m['marker'].loc[k]
+                    xk = data_m['PC2'].loc[k]
+                    yk = data_m['PC3'].loc[k]
+                    ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
+                                          edgecolors=fk, s=200, alpha=0.5,
+                                          linewidths=1)
                 colors = ['#D55E00', '#0072B2', '#F0E442']
                 for c in colors:
                     trans_group = data_m[data_m['color'] == c]
@@ -375,16 +306,17 @@ def Sensitivityplot(datalist, tex, label):
                                        ax[i, j], n_std=2, facecolor='none',
                                        edgecolor=c, alpha=1, lw=2)
                 for k in range(len(data_a)):
-                    fk = data_a['color'].loc[k]
-                    mk = data_a['marker'].loc[k]
-                    xk = data_a['PC2'].loc[k]
-                    yk = data_a['PC3'].loc[k]
                     if data_a['author'].loc[k] == 'Nartiss_2017':
-                        sk = 1000
-                    elif data_a['author'].loc[k] != 'Nartiss_2017':
-                        sk = 200
-                    ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
-                                         edgecolors='k', s=sk, alpha=1,
+                        fk = data_a['color'].loc[k]
+                        mk = data_a['marker'].loc[k]
+                        xk = data_a['PC2'].loc[k]
+                        yk = data_a['PC3'].loc[k]
+                    # if data_a['author'].loc[k] == 'Nartiss_2017':
+                    #     sk = 1000
+                    # elif data_a['author'].loc[k] != 'Nartiss_2017':
+                    #     sk = 200
+                        ax[i, j].scatter(xk, yk, marker=mk, facecolors=fk,
+                                         edgecolors='k', s=500, alpha=1,
                                          linewidths=1)
                 if j == 0:
                     ax[i, j].set_ylabel('PC3', size=20)
